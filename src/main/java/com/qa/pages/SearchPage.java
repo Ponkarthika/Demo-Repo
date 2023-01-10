0cb9ba7a3f53f6069712221d0a10602aa3c476c3
pon-16388 <pon-16388@csez.zohocorpin.com>
2023-01-10 18:22:55 +0530

    added search page feature

---
+++ src/main/java/com/qa/pages/SearchPage.java
@@ -0,0 +1,14 @@
+package com.qa.pages;
+
+public class SearchPage {
+	
+	public void searchPage() {
+		System.out.println("Search page");
+		
+	}
+	
+	public void loginSet() {
+		System.out.println("Login set");
+	}
+
+}
